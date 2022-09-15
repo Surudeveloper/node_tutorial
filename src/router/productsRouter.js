fe1d51142d84351a08141f9fc892afd397418ae5
Suresh Kumar <sksuresh348@gmail.com>
2022-09-15 13:45:35 +0530

    initial commit

--- src/router/productsRouter.js
+++ src/router/productsRouter.js
@@ -6,35 +6,52 @@ var url = process.env.mongoUrl
 function router(menu){
     productsRouter.route('/')
        .get(function(req,res){
-        mongodb.connect(url, function(err,dc){
-            if (err){
-                  res.status(501).send('Error while connecting')
-               }else{
-                var dbObj = dc.db('decnode')
-                dbObj.collection('products').find().toArray(function(err,response){
-                    if(err){
-                        res.status(501).send('Error while fetching product')
-                    }else{
-                        res.send(response)
-                    }
-                })
-               }
-        })
-       
-   })
+    })
 
     productsRouter.route('/category/:id')
        .get(function(req,res){
     //    var id = req.params.id; or
        var {id} = req.params
        var name = req.query.name;
-       res.render('products',{title:'Products Page',data:[],menu}) 
-   })   
+       console.log(">>>>id",id);
+       mongodb.connect(url, function(err,dc){
+        if (err){
+              res.status(501).send('Error while connecting')
+           }else{
+                 var dbObj = dc.db('decnode')
+                     dbObj.collection('products').find({'category_id':Number(id)}).toArray(function(err,response){
+                if(err){
+                    res.status(501).send('Error while fetching product')
+                }else{
+                    // res.send(response)
+                      res.render('products',{title:'Products Page',data:response,menu}) 
+                    }
+            })
+           }
+        })
+      
+    })   
 
-    productsRouter.route('/details')
+    productsRouter.route('/details/:id')
        .get(function(req,res){
-       res.send('Products Details')
-   })
+        var {id} = req.params
+        console.log(">>>>id",id);
+        mongodb.connect(url, function(err,dc){
+         if (err){
+               res.status(501).send('Error while connecting')
+            }else{
+                  var dbObj = dc.db('decnode')
+                      dbObj.collection('products').find({'id':Number(id)}).toArray(function(err,response){
+                 if(err){
+                     res.status(501).send('Error while fetching product')
+                 }else{
+                     // res.send(response)
+                       res.render('productDetails',{title:'Products Detail Page',data:response,menu}) 
+                     }
+             })
+            }
+         })
+    })
    return productsRouter
 }
 module.exports = router   
